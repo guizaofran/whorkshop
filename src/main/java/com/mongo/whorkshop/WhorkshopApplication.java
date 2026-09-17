bc95c372e9539c325854a5dd98202cd3e4052d37
Guilherme Franklin <gui.franklin17@gmail.com>
2026-09-17 19:08:17 -0300

    first commit

---
+++ src/main/java/com/mongo/whorkshop/WhorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.whorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongo/whorkshop/WhorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.whorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
