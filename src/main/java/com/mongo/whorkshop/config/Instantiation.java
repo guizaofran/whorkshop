4aaf2ac7ce7c358af1348fd74a937012446b535b
Guilherme Franklin <gui.franklin17@gmail.com>
2026-09-22 21:35:45 -0300

    instanciação da base de dados

---
+++ src/main/java/com/mongo/whorkshop/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.mongo.whorkshop.config;
+
+import com.mongo.whorkshop.domain.User;
+import com.mongo.whorkshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null,"Maria","maria@gmail.com");
+        User alex = new User(null,"Alex","alex@gmail.com");
+        User bob = new User(null,"Bob","bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+    }
+}
