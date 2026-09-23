5605bf3149536273d483b24b748278913f216ab2
Guilherme Franklin <gui.franklin17@gmail.com>
2026-09-22 21:55:01 -0300

    userDTO

--- src/main/java/com/mongo/whorkshop/resources/UserResource.java
+++ src/main/java/com/mongo/whorkshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.mongo.whorkshop.resources;
 
 import com.mongo.whorkshop.domain.User;
+import com.mongo.whorkshop.dto.UserDTO;
 import com.mongo.whorkshop.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserServices userServices;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userServices.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
